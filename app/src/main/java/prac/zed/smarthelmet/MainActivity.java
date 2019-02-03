ab4e5a81eb15bf700cfbf8a4f18bd6fdbcda80db
Karthik <hkarthik97@gmail.com>
2019-02-03 09:46:24 +0530

    added some functions like Onclick

--- app/src/main/java/prac/zed/smarthelmet/MainActivity.java
+++ app/src/main/java/prac/zed/smarthelmet/MainActivity.java
@@ -2,21 +2,60 @@ package prac.zed.smarthelmet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button buttonRegister  ;
     private EditText editTextPassword;
     private EditText editTextEmail;
-    private TextView textViewSignup;
-    
+    private TextView textViewSignin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonRegister =(Button) findViewById(R.id.buttonRegister);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewSignin = (TextView) findViewById(R.id.textViewSignin);
+        //setting on click listener
+        buttonRegister.setOnClickListener(this);
+        textViewSignin.setOnClickListener(this);
+
 
     }
+    private void buttonRegister(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        if(TextUtils.isEmpty(email)){
+            //Shows toast when the email field is left empty
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+            
+        }
+
+    }
+    @Override
+    public void onClick(View view){
+
+        if(view == buttonRegister){
+            buttonRegister();
+        }
+        if(view == textViewSignin){
+            textViewSignin();
+        }
+
+
+    }
+
 }
